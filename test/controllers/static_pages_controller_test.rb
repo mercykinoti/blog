fc32dee4b0b787b1135a0c3a1534205ba42fb6e3
mkin <mkinoti69@gmail.com>
2016-12-20 09:13:22 +0000

    first commit

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase	
+  test "should get root" do
+    get FILL_IN
+    assert_response FILL_IN
+  end
+  
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Home"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+     assert_select "title", "contact"
+  end
+
+  test "should get about" do
+  	get :about
+  	assert_response :success
+  	 assert_select "title", "about"
+  end
+
+end
